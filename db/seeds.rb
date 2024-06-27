bbe793516f82b2539d775b4fe26eaf2153539fed
Yannan Sun <yannansun0723@gmail.com>
2024-06-27 17:05:31 +1000

    rails yelp mvp done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
+# This file should ensure the existence of records required to run the application in every environment (production
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+Restaurant.destroy_all
+
+categories = %w[chinese italian japanese french belgian]
+
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: categories.sample
+  )
+end
